04f54ee01e9dafe4de36cee0b0b54d4022f5e795
ueg1990 <uehtesham90@gmail.com>
2017-02-28 21:46:03 -0500

    Define main entry point for creating Flask app

--- app/app.py
+++ app/app.py
@@ -1,9 +1,36 @@
-from flask import Flask
-app = Flask(__name__)
+"""
+Main app module
+"""
+from flask import Flask, render_template
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+from app.config import ProdConfig
+from app.extensions import db, login_manager
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+def create_app(config_object=ProdConfig):
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+    register_extensions(app)
+    register_blueprints(app)
+    register_error_handlers(app)
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+    login_manager.init_app(app)
+
+
+def register_blueprints(app):
+    pass
+
+
+def register_error_handlers(app):
+    """Register error handlers."""
+    def render_error(error):
+        """Render error template."""
+        # If a HTTPException, pull the `code` attribute; default to 500
+        error_code = getattr(error, 'code', 500)
+        return render_template('errors/{0}.html'.format(error_code)), error_code
+    for errcode in [401, 404, 500]:
+        app.errorhandler(errcode)(render_error)
