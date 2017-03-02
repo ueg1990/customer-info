e5510fa8e39507f5bc204b4e3f4e03d73f147523
ueg1990 <uehtesham90@gmail.com>
2017-03-02 01:00:50 -0500

    Define basic views and templates for public blueprint

--- app/customer/views.py
+++ app/customer/views.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 
+from app.decorators import login_required
+
 blueprint = Blueprint('customer', __name__, url_prefix='/customer')
 
 
 @blueprint.route('/')
+@login_required
 def customers():
     """List customers."""
     return render_template('customer/index.html')
--- app/public/views.py
+++ app/public/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, session, url_for
+
+from app.decorators import login_required
 
 blueprint = Blueprint('public', __name__)
 
@@ -6,4 +8,17 @@ blueprint = Blueprint('public', __name__)
 @blueprint.route('/')
 def home():
     """Return Home Page"""
-    return 'Hello World!' 
+    return render_template('public/index.html')
+
+
+@blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    """Return Login page"""
+    session['logged_in'] = True
+    return render_template('public/login.html')
+
+
+@blueprint.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('home'))
