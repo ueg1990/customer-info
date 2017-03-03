50ff0c05c7a304dc45d24c37257c94094bc09570
ueg1990 <uehtesham90@gmail.com>
2017-03-03 01:50:36 -0500

    Define model for Customer

---
+++ app/customer/models.py
@@ -0,0 +1,27 @@
+from app.extensions import db
+
+
+class Customer(db.Model):
+    __tablename__ = 'customers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+    phone_number = db.Column(db.String(10), unique=True)
+    address = db.Column(db.Text)
+    last_order = db.Column(db.Text)
+    send_email = db.Column(db.Boolean)
+
+    def __init__(self, first_name, last_name, email, phone_number, address,
+                 last_order, send_email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.address = address
+        self.last_order = order
+        self.send_email = send_email
+
+    def __repr__(self):
+        return '<Cusomter {!r} {!r}>'.format(self.first_name, self.last_name)
