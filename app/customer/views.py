d0e0e6281e09aa7a8b8b3f699320028589cc7c0e
ueg1990 <uehtesham90@gmail.com>
2017-03-05 23:02:55 -0500

    Add lots of functionality

--- app/customer/views.py
+++ app/customer/views.py
@@ -1,12 +1,54 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 
+
+from app.customer.forms import CustomerSearchForm, NewCustomerForm
+from app.customer.models import Customer
 from app.decorators import login_required
 
 blueprint = Blueprint('customer', __name__, url_prefix='/customer')
 
 
-@blueprint.route('/')
+@blueprint.route('/', methods=('GET', 'POST'))
+@login_required
+def home():
+    """Logged-in user homepage"""
+    error = None
+    form = CustomerSearchForm()
+    if form.validate_on_submit():
+        phone_number = form.phone_number.data
+        customer = Customer.query.filter_by(phone_number=phone_number).first()
+        if customer:
+            return render_template('customer/customer.html', customer=customer)
+        else:
+            error = 'Customer with phone number {} not found'.format(phone_number)
+    return render_template('customer/index.html', form=form, error=error)
+
+
+@blueprint.route('/all')
 @login_required
 def customers():
     """List customers."""
     return render_template('customer/index.html')
+
+
+@blueprint.route('/customer/<int:customer_id>')
+@login_required
+def customer(customer_id):
+    """Get customer by id"""
+    customer = Customer.query.get(customer_id)
+    return render_template('customer/index.html')
+
+
+@blueprint.route('/customer', methods=('GET', 'POST'))
+@login_required
+def add_customer():
+    """Add new customer"""
+    form = NewCustomerForm()
+    if form.validate_on_submit():
+        customer = Customer(form.first_name.data, form.last_name.data, form.email.data,
+                            form.phone_number.data, form.address.data, form.last_order.data,
+                            form.send_emai.data)
+        db.session.add(customer)
+        db.session.commit()
+        return render_template('customer/customer.html', customer=customer)
+    return render_template('customer/index.html', form=form)
--- app/public/views.py
+++ app/public/views.py
@@ -1,10 +1,11 @@
 import os
 from flask import Blueprint, redirect, render_template, request, session, url_for
+from werkzeug.security import check_password_hash
 
 from app.decorators import login_required
 
-ADMIN_USERNAME = os.environ['CUSTOMER_INFO_ADMIN_USERNAME']
-ADMIN_PASSWORD_HASH = os.environ['CUSTOMER_INFO_ADMIN_PASSWORD_HASH']
+ADMIN_USERNAME = 'user' #os.environ['CUSTOMER_INFO_ADMIN_USERNAME']
+ADMIN_PASSWORD_HASH = 'pbkdf2:sha1:1000$Z6h2m5v4$10c6f4bae53c8f9eacb61bbb6922347b87419fba' #os.environ['CUSTOMER_INFO_ADMIN_PASSWORD_HASH']
 
 blueprint = Blueprint('public', __name__)
 
@@ -18,10 +19,14 @@ def home():
         password = request.form['password']
         if _validate_credentials(username, password):
             session['logged_in'] = True
-            return redirect(url_for('customer/index.html'))
+            return redirect(url_for('customer.home'))
         else:
             error = 'Invalid username or password'
-    return render_template('public/index.html', error=None)
+    # If user logs in and immediately clicks Back button, should be redirected
+    # to home page of a logged in user
+    if session.get('logged_in'):
+        return redirect(url_for('customer.home'))
+    return render_template('public/index.html', error=error)
 
 
 def _validate_credentials(username, password):
@@ -33,4 +38,4 @@ def _validate_credentials(username, password):
 @login_required
 def logout():
     session.pop('logged_in', None)
-    return redirect(url_for('home'))
+    return redirect(url_for('public.home'))
