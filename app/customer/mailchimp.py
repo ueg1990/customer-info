950eb8ba50cab62f2696ecafe269880c7a484588
ueg1990 <uehtesham90@gmail.com>
2017-03-18 21:41:57 -0400

    Fix exception handling

--- app/customer/mailchimp.py
+++ app/customer/mailchimp.py
@@ -12,29 +12,25 @@ MAILCHIMP_API_KEY = os.environ.get('MAILCHIMP_API_KEY')
 MAILCHIMP_LIST_ID = os.environ.get('MAILCHIMP_LIST_ID')
 MAILCHIMP_USERNAME = os.environ.get('MAILCHIMP_USERNAME')
 
-
-class MailChimpException(requests.exceptions.RequestException):
-    pass
+mailchimp_client = MailChimp(MAILCHIMP_USERNAME, MAILCHIMP_API_KEY)
 
 
 def add_member_to_subscription_list(email):
     """Add given email_address to MailChimp subscription list"""
-    client = MailChimp(MAILCHIMP_USERNAME, MAILCHIMP_API_KEY)
     try:
-        client.lists.members.create(list_id=MAILCHIMP_LIST_ID, data={'email_address': email,
-                                    'status': 'subscribed'})
-    except MailChimpException:
+        mailchimp_client.lists.members.create(list_id=MAILCHIMP_LIST_ID,
+                                              data={'email_address': email,
+                                              'status': 'subscribed'})
+    except requests.exceptions.RequestException:
         pass
     else:
-        return client.lists.members.subscriber_hash
-
+        return mailchimp_client.lists.members.subscriber_hash
 
 
 def remove_member_from_subscription_list(mailchimp_subscriber_hash):
     """Remove cusotmer from MailChimp subsription list"""
-    client = MailChimp(MAILCHIMP_USERNAME, MAILCHIMP_API_KEY)
     try:
-        client.lists.members.delete(list_id=MAILCHIMP_LIST_ID,
-                                    subscriber_hash=mailchimp_subscriber_hash)
-    except MailChimpException:
+        mailchimp_client.lists.members.delete(list_id=MAILCHIMP_LIST_ID,
+                                              subscriber_hash=mailchimp_subscriber_hash)
+    except requests.exceptions.RequestException:
         pass
